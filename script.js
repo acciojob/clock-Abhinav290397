1167b2275172795cc175d706d2959cd60417acf5
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-15 18:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const P = document.querySelector("#timer");
+function updateTime(){
+	let date = new Date();
+	P.innerText = date;
+}
+setInterval(updateTime,1000);
